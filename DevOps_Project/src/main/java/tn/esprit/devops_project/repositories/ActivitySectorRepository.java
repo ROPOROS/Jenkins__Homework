d248fbc2241e087d4fd0649919a4e6bc46a9cd7a
Chebbi_raed <61346201+ROPOROS@users.noreply.github.com>
2023-10-30 15:57:40 +0100

    Docker-Compose

--- DevOps_Project/src/main/java/tn/esprit/devops_project/repositories/ActivitySectorRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.devops_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.devops_project.entities.ActivitySector;
-
-public interface ActivitySectorRepository extends JpaRepository<ActivitySector, Long> {
-}
